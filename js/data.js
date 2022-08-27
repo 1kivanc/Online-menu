6f32dcc22229858653e1f5871a879a2590d60e52
1kivanc <kivanc0556@gmail.com>
2022-08-27 23:14:01 +0300

    online-menu

---
+++ js/data.js
@@ -0,0 +1,112 @@
+const menu = [
+    {
+        id:1,
+        title:'Pizza Bol (Büyük + Orta boy) + Coca-Cola (1L)',
+        category: 'pizza',
+        img:'images/buyukboy+ortaboy.webp',
+        desc:"Seçeceğiniz Büyük Boy Pizza + Seçeceğiniz Orta Boy Pizza + Patates Kızartması + 10'lu Çıtır Tavuk Topları + Coca-Cola (1 L.)",
+        price:186
+    },
+    {
+      id:2,
+      img:'images/doyuranmenu.webp',
+      title:'Doyuran Menu',
+      category:'pizza',
+      desc:'Seçeceğiniz Büyük Boy Pizza + Çıtır Patates + İçecek (1 L.)',
+      price:104
+    },
+    {
+      id:3,
+      img:'images/ortaboy.webp',
+      title:'Orta Boy Pizzalar',
+      category:'pizza',
+      desc:'Cazip pizzalar için geçerlidir. Özel pizzalar için 7 TL, Bol Malzeme pizzalar için 14 TL, Trend pizzalar için 21 TL, Gurme pizzalar için 28 TL fiyat farkı alınmaktadır.',
+      price:84.99
+    },
+    {
+      id:4,
+      img:'images/hamburger.webp',
+      title:'Tavuk Burger menu',
+      category:'hamburger',
+      desc:'Tavuk burger + kola(330ml) + cips',
+      price:55
+    },
+    {
+        id:5,
+        img:'images/cheeseburber.webp',
+        title:'Cheeseburger',
+        category:'hamburger',
+        desc:'120 gr burger köftesi, domates, marul , cheddar peyniri',
+        price:35
+    },
+    {
+        id:6,
+        img:'images/tekkisilikalgidamenu.webp',
+        title:'Tek Kişilik Algida Menü',
+        category:'hamburger',
+        desc:'Double Cheeseburger + Orta Boy Patates + Seçceğiniz Algida Max + Kutu İçecek',
+        price:67
+    },
+    {
+        id:7,
+        img:'images/kralikili.webp',
+        title:'Kral İkili Menu',
+        category:'hamburger',
+        desc:'Big King® Sandviç + King Chicken® Sandviç + Patates Kızartması (Orta) + 1 L. İçecek',
+        price:99
+    },
+    {
+        id:8,
+        img:'images/su.webp',
+        title:'Su',
+        category:'drink',
+        desc:'500 ml',
+        price:6
+    },
+    {
+        id:9,
+        img:'images/kolakucuk.webp',
+        title:'Coca-Cola Zero Sugar (Büyük)',
+        category:'drink',
+        desc:'500 ml',
+        price:12
+    },
+    {
+        id:10,
+        img:'images/fusetea.webp',
+        title:'Fuse-Tea Şeftali (orta)',
+        category:'drink',
+        desc:'400 ml',
+        price:10
+    },
+    {
+        id:11,
+        img:'images/sprite.webp',
+        title:'Sprite',
+        category:'drink',
+        desc:'330 ml',
+        price:17
+    },
+    {
+        id:12,
+        img:'images/tekerayran.webp',
+        title:'Eker Ayran',
+        category:'drink',
+        desc:'295 ml',
+        price:15
+    },
+    {
+        id:13,
+        img:'images/soda.webp',
+        title:'Soda',
+        category:'drink',
+        desc:'300 ml',
+        price:5
+    },
+
+    
+    
+    
+    
+    
+]
---
+++ js/script.js
@@ -0,0 +1,89 @@
+
+
+const optionMenu = document.querySelector('.select-menu'),
+      selectBtn = optionMenu.querySelector('.select-cat'),
+      options= optionMenu.querySelectorAll('.option'),
+      btn_text= optionMenu.querySelector('.btn-text');
+
+selectBtn.addEventListener('click' , () => optionMenu.classList.toggle('active'));
+
+options.forEach(option => {
+  option.addEventListener('click',() => {
+    let selectedOption = option.querySelector('.option-text').innerText;
+    btn_text.innerText = selectedOption;
+    optionMenu.classList.remove('active');
+  })
+})      
+
+const itemList = document.querySelector(".items-list");
+
+const createFoods = (food) => {
+    let html = `
+    <div class="card">
+        <img src="${food.img}" alt="">
+        
+      
+            <div class="card-info">
+                <h3 class="title">${food.title}</h3>
+                <p class="price">${food.price} <small>TL</small></p>
+                <p class="description">${food.desc}</p>
+                <button type="button" class="addCart"><i class="fa-solid fa-plus"></i></button>
+           
+             </div>
+        </div>
+    `
+    return html;
+    
+}
+
+
+const listAllFoods = () => {
+    let allFoods="";
+  
+    menu.map(item => {
+      allFoods += createFoods(item)
+    })
+    itemList.innerHTML = allFoods;
+  }
+  
+document.addEventListener("DOMContentLoaded",listAllFoods);
+
+const listPizza = () => {
+  let pizza = "";
+
+  menu.map(item => {
+    if(item.category === "pizza"){
+      pizza += createFoods(item)
+    }
+  })
+  itemList.innerHTML = pizza;
+  
+}
+
+document.querySelector('.select-menu #pizza').addEventListener('click',listPizza);
+
+const listBurger = () => {
+  let burger = "";
+
+  menu.map(item => {
+    if(item.category === "hamburger"){
+      burger += createFoods(item)
+    }
+  })
+  itemList.innerHTML = burger;
+}
+
+document.querySelector('.select-menu #burger').addEventListener('click',listBurger);
+
+const listDrink = () => {
+  let drink = "";
+
+  menu.map(item => {
+    if(item.category === "drink"){
+      drink += createFoods(item)
+    }
+  })
+  itemList.innerHTML = drink;
+}
+
+document.querySelector('.select-menu #drink').addEventListener('click',listDrink);
